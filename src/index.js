0bdf9a7d5dec0644836522706248a3de16187662
Leonardo Veiga <leo14s@gmail.com>
2019-04-16 23:09:13 -0300

    criando e listanto tweets

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.send("Hello World");
+mongoose.connect("mongodb://localhost:27017/goweek5", {
+  useNewUrlParser: true
 });
+
+app.use(express.json());
+app.use(require("./routes"));
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
